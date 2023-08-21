cafb935ee7cf36bc3877a5f003cd3fcc1c89d253
efoneli <abbaefoneli@gmail.com>
2023-08-21 13:12:57 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+  // const getWhale = () => {
+  //   fetch('https://cat-fact.herokuapp.com/facts/random')
+  // .then(response => response.json())
+  // .then(data => console.log(data.text))
+  // .catch(error => console.error(error));
+  // }
+
+  // console.log(getWhale())
+
+  // const time = () => {
+    // let day = date.getDay();
+    // let month = date.getMonth() + 1;
+    // let year = date.getFullYear(); 
+    
+    // if (date.getDate()) {
+    //   return <Text>Today</Text>
+    // } else if(date.getDate() + 1) {
+    //   return <Text>Tomorow</Text>
+    // } else if (date.getDate())
+  
+
+  // const date = new Date(15/8/2023);
+  // let today = date.getDay();
+
+  // console.log(today)
+//     let tomorow = date.getDay() + 1;
+//     let someday = date.getDay() + 2; 
+// if (date === today) {
+// console.log('Today yay!')
+// }else if (date === tomorow) {
+//   console.log('tomorrow yay!')
+// } else {
+//   console.log('someday yay!')
+// }
+  // }
+
+  // let date = '20/08/2023'
+
+  console.log(new Date())
